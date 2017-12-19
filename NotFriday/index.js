6e284fd87f0009b08e2c60aff200724f50661cc5
Guillaume Lacasa <glacasa@users.noreply.github.com>
2017-12-19 11:32:17 +0100

    Uncomment demo code

--- NotFriday/index.js
+++ NotFriday/index.js
@@ -4,25 +4,22 @@ function failTask(message) {
     task.setResult(task.TaskResult.Failed, message);
 }
 
-// var checkfriday = task.getBoolInput('checkfriday', false);
-// if (checkfriday) {
-//     var today = new Date();
-//     if (today.getDay() === 5) {
-//         failTask("On ne déploie pas, c'est vendredi !");
-//     }
-//     if (today.getDay() === 1) {
-//         failTask("Doucement, c'est que lundi, on déploiera demain");
-//     }
-// }
+var checkfriday = task.getBoolInput('checkfriday', false);
+if (checkfriday) {
+    var today = new Date();
+    if (today.getDay() === 5) {
+        failTask("On ne déploie pas, c'est vendredi !");
+    }
+}
 
 
-// var ferie = require('./ferie');
-// var checkholiday = task.getBoolInput('checkholiday', false);
-// if (checkholiday) {
-//     var today = Date.UTC();
-//     var tomorrow = new Date(today + 86400000);
+var ferie = require('./ferie');
+var checkholiday = task.getBoolInput('checkholiday', false);
+if (checkholiday) {
+    var today = Date.UTC();
+    var tomorrow = new Date(today + 86400000);
 
-//     if (ferie.estFerie(tomorrow)) {
-//         failTask("Demain c'est férié, pas la peine de déployer");
-//     }
-// }
+    if (ferie.estFerie(tomorrow)) {
+        failTask("Demain c'est férié, pas la peine de déployer");
+    }
+}
